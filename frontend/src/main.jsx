601f54ed5ba98b623608df3980bee3cc9f6812a1
sensologica <119978540+sensologica@users.noreply.github.com>
2023-11-20 17:18:05 +0100

    Organize folders, refactror static HTML pages into React components and React Router routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { 
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from "react-router-dom"
+import Homepage from './pages/Homepage.jsx'
+import SignIn from './pages/SignIn.jsx'
+import User from './pages/User.jsx'
+import "./main.css"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route 
+        path="/"
+        element={<Homepage />}
+        // errorElement={<RoutingError />}
+      />
+      <Route 
+        path="/sign-in"
+        element={<SignIn />}
+        // errorElement={<RoutingError />}
+      />
+      <Route 
+        path="/user"
+        element={<User />}
+        // errorElement={<RoutingError />}
+      />
+      {/* <Route element={<PageLayout />}>
+        <Route
+          path="/user/:userId"
+          element={<Dashboard />}
+          errorElement={<RoutingError />}
+          loader={
+            ({ params }) => {
+              const userId = parseInt(params.userId)
+              return isNaN(userId) 
+                ? console.error("Error: Invalid user ID.")
+                : userId
+            }
+          }
+        />
+      </Route> */}
+    </>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
