c257594c346422525360ba2053051a7211ad0fa0
sensologica <119978540+sensologica@users.noreply.github.com>
2023-12-05 09:12:27 +0100

    Remove unnecessary comment

--- frontend/src/components/ButtonSubmitForm.jsx
+++ frontend/src/components/ButtonSubmitForm.jsx
@@ -7,17 +7,3 @@ const ButtonSubmitForm = () => {
 }
 
 export default ButtonSubmitForm
-
-
-        // Navigate to Profile
-
-      // Grab token from localStorage in the Profile component, then do the operations with the token.
-
-      // fetch("http://localhost:3001/api/v1/user/profile", {
-      //   method: "POST",
-      //   headers: new Headers({
-      //     "Authorization": `Bearer ${localStorage.getItem("jwt-token")}`
-      //   })
-      // })
-      //   .then(response => response.json())
-      //   .then(data => console.log(data))
